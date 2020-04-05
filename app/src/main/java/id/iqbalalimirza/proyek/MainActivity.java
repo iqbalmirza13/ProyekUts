030033502c479f37ef190d0fddc06a3dd34a8b17
iqbalmirza13 <iqbalmirza1214@gmail.com>
2020-04-05 16:11:30 +0700

    memindahkan activity dari main ke biodata

--- app/src/main/java/id/iqbalalimirza/proyek/MainActivity.java
+++ app/src/main/java/id/iqbalalimirza/proyek/MainActivity.java
@@ -16,6 +16,10 @@ import id.iqbalalimirza.proyek.model.model_pemain;
 public class MainActivity extends AppCompatActivity implements ListPemainAdapter.OnItemListener{
     private RecyclerView rvPemain;
     private ArrayList<model_pemain> list = new ArrayList<>();
+    public static final String NAMA_KEY ="yudo";
+    public static final String DETAIL_KEY = "asd";
+    public static final String FOTO_KEY = "1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +33,15 @@ public class MainActivity extends AppCompatActivity implements ListPemainAdapter
         rvPemain.setAdapter(listPemainAdapter);
     }
 
-    private void showRecyclerList(){
-        rvPemain.setLayoutManager(new LinearLayoutManager(this));
-        ListPemainAdapter listPemainAdapter = new ListPemainAdapter(list);
-        rvPemain.setAdapter(listPemainAdapter);
-
-    }
-
     @Override
     public void onListClicked(int index, model_pemain item) {
         Intent intent = new Intent(this, biodata.class);
         String nama = item.getNama();
         String biodata = item.getBiodata();
-        intent.putExtra("detail_nama",nama);
-        intent.putExtra("detail_biodata", biodata);
+        int foto = item.getFoto_pemain();
+        intent.putExtra(FOTO_KEY, foto);
+        intent.putExtra(NAMA_KEY,nama);
+        intent.putExtra(DETAIL_KEY, biodata);
         startActivity(intent);
     }
 }
--- app/src/main/java/id/iqbalalimirza/proyek/biodata.java
+++ app/src/main/java/id/iqbalalimirza/proyek/biodata.java
@@ -8,6 +8,9 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,22 +20,27 @@ public class biodata extends AppCompatActivity {
 
     private TextView textBiodata;
     private ImageView imageView;
-    private TextView name;
+    private TextView nama;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biodata);
-        name= findViewById(R.id.text_name);
+        nama= findViewById(R.id.nama_pemain);
         textBiodata=findViewById(R.id.biodata_pemain);
         imageView=findViewById(R.id.foto_pemain);
 
+
+
         Bundle extras = getIntent().getExtras();
         if (extras != null){
-            name.setText(extras.getString("detail_nama"));
-            textBiodata.setText(extras.getString("detail_biodata"));
-
+            nama.setText(extras.getString(MainActivity.NAMA_KEY));
+            textBiodata.setText(extras.getString(MainActivity.DETAIL_KEY));
+            Glide.with(this)
+                    .load((extras.getInt(MainActivity.FOTO_KEY)))
+                    .apply(new RequestOptions().override(350,550))
+                    .into(imageView);
         }
-
     }
 }
